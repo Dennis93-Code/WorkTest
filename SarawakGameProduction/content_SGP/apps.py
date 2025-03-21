afa19916c2a1e334b52a2d93ee15672557410d44
Dennis93-Code <denniswong@karunatech.com.my>
2025-03-21 11:53:59 +0800

    Remove junks and reinitiate odoo to use odoo primarily

--- SarawakGameProduction/content_SGP/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-class ContentSgpConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'content_SGP'
--- SarawakGameProduction/odoo_management_sgp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class OdooManagementSgpConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'odoo_management_sgp'
